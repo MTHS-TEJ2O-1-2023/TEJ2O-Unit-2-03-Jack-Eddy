7d1cc294847ce0dc86e3289bc7d7e1cd9ff144be
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-09-19 13:28:12 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack Eddy
+ * Created on: Sep 2023
+ * This program can do basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('40 + 2 =' + (40 + 2).toString())
