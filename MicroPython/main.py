67127a7b7a594733e70c6020c570a57e4cfabfd4
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-09-20 17:27:34 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Jack
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("A rectangle has dimensions 5 cm & 3 cm")
+
+display.scroll("Perimeter would be :" + str(5 * 2 + 3 * 2))
+
+display.scroll("cm")
+
+display.scroll("Area would be :" + str(5 * 3))
+
+display.scroll("cm")
